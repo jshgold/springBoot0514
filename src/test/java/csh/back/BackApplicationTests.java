65f549a9801ce84759d51bb0e98f824892bcb12c
devho <jshgold93@gmail.com>
2026-05-14 12:32:29 +0900

    init project

---
+++ src/test/java/csh/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package csh.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot0514ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
